23a8d163e52c09ea1e611b7562eae6250fdf4d94
hsimj001 <hsimj001@ucr.edu>
2019-10-21 20:06:22 -0700

    Create support for google sign-in and sign-out

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATUa7K3V5EHbXSIj3vbd1xUW1PNAd08EY",
+    authDomain: "cs180-keep.firebaseapp.com",
+    databaseURL: "https://cs180-keep.firebaseio.com",
+    projectId: "cs180-keep",
+    storageBucket: "cs180-keep.appspot.com",
+    messagingSenderId: "216350288631",
+    appId: "1:216350288631:web:f862b60c71a8e09c32c1f2"
+  };
+
+export default firebaseConfig;
