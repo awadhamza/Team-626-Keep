c91e78bd020084bfa8ea8ca881d657679ec15716
awadhamza <34406043+awadhamza@users.noreply.github.com>
2019-10-27 15:20:35 -0700

    Correct upload directory

--- src/App.js
+++ src/App.js
@@ -12,7 +12,62 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 var signed_in = false;
 
 class App extends Component{
-  render() {
+    
+    testFunction(x) {
+        //var y = "signed in = " + x.toString();
+        alert(x);
+    }
+    
+    lockTabs() {
+        var collection = document.getElementsByClassName('active_tabs');
+        if(collection != null){
+            /*alert("INITIAL COLL. LENGTH = " + collection.length);
+            */
+            for(var i = collection.length - 1; i >= 0; i--){
+                //alert(i + " " + collection[i].className.toString());
+                //alert(collection[i].textContent);
+                collection[i].className = "disabled_tabs";
+            }
+            
+            
+            window.onload = function(){
+                document.getElementById("1_act").innerHTML = "<i class=\"fas fa-lock\"></i> My Notes";
+                document.getElementById("2_act").innerHTML = "<i class=\"fas fa-lock\"></i> Create Note";
+                document.getElementById("3_act").innerHTML = "<i class=\"fas fa-lock\"></i> Archives ";
+            }
+            
+            //        collection[2].className = "disabled_tabs";
+            //        collection[1].className = "disabled_tabs";
+            //        collection[0].className = "disabled_tabs";
+        }
+    }
+    
+    unlockTabs() {
+        var collection = document.getElementsByClassName('disabled_tabs');
+        if(collection != null){
+            for(var i = collection.length - 1; i >= 0; i--){
+                collection[i].className = "active_tabs";
+            }
+        }
+        
+        document.getElementById("1_act").innerHTML = "<i class=\"fas fa-sticky-note\"></i> My Notes";
+        document.getElementById("2_act").innerHTML = "<i class=\"fas fa-plus-square\"></i> Create Note";
+        document.getElementById("3_act").innerHTML = "<i class=\"fas fa-inbox\"></i> Archives ";
+    }
+    
+    updateConnection(x) {
+        if(x === 1) {
+            //testFunction("signed in!");
+            this.unlockTabs();
+            signed_in = true;
+        }
+        else {
+            this.lockTabs();
+            signed_in = false;
+        }
+    }
+    
+    render() {
     const {
       user,
       signOut,
@@ -21,47 +76,65 @@ class App extends Component{
   return (
     <div className="App">
       <header className="App-header">
-        <div className="nav-bar" onShow="lockTabs()">
+        <div className="nav-bar">
             <img src={logo} className="App-logo" alt="logo" />
-            <a id="nav_tabs" href="">My Notes</a>
-            <a id="nav_tabs" href="">Create Note</a>
-            <a id="nav_tabs" href="">Archives </a>
+            {
+                user
+                ?
+                 <React.Fragment>
+                    <a class="active_tabs" id="1_act" href="#"><i class="fas fa-plus-square"></i> My Notes</a>
+                    <a class="active_tabs" id="2_act" href="#"><i class="fas fa-plus-square"></i> Create Note</a>
+                    <a class="active_tabs" id="3_act" href="#"><i class="fas fa-inbox"></i> Archives </a>
+                 </React.Fragment>
+                :
+                 <React.Fragment>
+                    <a class="disabled_tabs" id="1_act" href="#"><i class="fas fa-lock"></i> My Notes</a>
+                    <a class="disabled_tabs" id="2_act" href="#"><i class="fas fa-lock"></i> Create Note</a>
+                    <a class="disabled_tabs" id="3_act" href="#"><i class="fas fa-lock"></i> Archives </a>
+                 </React.Fragment>
+            }
+        {/*
+          user
+            ? <p> ONLINE </p>
+            : <p> OFFLINE </p>
+        */}           
         </div>
         {
           user 
             ? 
               <React.Fragment>
+                  {this.updateConnection(1)}    
                   <React.Fragment>
-                    <img src={signed_in_img}className="sign-in-logo"/> 
-                    <p onShow="updateConnection(1)">Hello, {user.displayName}</p>
+                    <img src={signed_in_img} className="sign-in-logo"/> 
+                    <p>Hello, {user.displayName}</p>
                   </React.Fragment>
               </React.Fragment>
-      
             : <React.Fragment>
-                <img src={sign_in_img}className="sign-in-logo"/> 
-                <p onShow="updateConnection(0)">Please sign in.</p>
+                <img src={sign_in_img} onload={this.updateConnection(0)} className="sign-in-logo"/> 
+                <p>Please sign in.</p>
+                {this.updateConnection(0)}
               </React.Fragment>
         }
+        {/*
+          signed_in
+            ? <p> ONLINE </p>
+            : <p> OFFLINE </p>
+        */}
         {
           user
             ? <button onClick={signOut}>Sign out</button>
             : <button onClick={signInWithGoogle}>Sign in with Google</button>
         }
+        
+        {/*
+        <div> TEST BUTTONS </div>
+        <button onClick={this.lockTabs}>LOCK TABS</button>
+        <button onClick={this.unlockTabs}>UNLOCK TABS</button>
+        */}
       </header>
     </div>
   );
 }
-}
-
-function lockTabs() {
-    alert(document.getElementById('[nav_tabs]'));
-}
-
-function updateConnection(x) {
-    if(x == 1){
-        return signed_in = true;
-    }
-    return signed_in = false;
 }
 
 const firebaseAppAuth = firebaseApp.auth();
